09c283aa191aa6b47bf7072542973d544d1795af
matias33pascual <matias33pascual@gmail.com>
2021-09-23 19:56:04 -0300

    pasando informacion entre elementos

--- main.c
+++ main.c
@@ -1,43 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-    /*
-        si esta vacia, retorno NULL
-        si tiene un solo nodo, retorna lista
-        si tiene mas de un nodo, tomo el 1er nodo, invierto lo que sigue y
-        lo agrego al final de la lista invertida
-
-        nos vamos a ayudar con la funcion agregar al final
-
-        primero tengo que desvicular al nodo de la lista
-        OJO no perder las referencias...
-    */
-nodo * invertirRecursivo (nodo * lista)
+/** FUNCIONES PARA PRACTICAR CORRECCION DE FUNCIONES */
+/*
+/// que esta mal en esta funcion?
+int inicPila (Pila pila)
 {
-    nodo * primero=NULL;
-
-    if(lista!=NULL)
-    {
-        primero=lista;  // guardo el primero nodo
-        lista=lista->siguiente;   // avanzo en la lista
-        primero->siguiente=NULL;  // desvinculo el primero nodo
-        lista=agregarFinal(invertirRecursivo(lista),primero);
-    }
-    return lista;
-
+    (*pila)=inicLista(pila);
 }
+
+errores:
+    valor de retorno: deberia void
+    inicLista no lleva parametros
+    el parametro Pila debe ir como puntero
+
+    corrrecion:
+        void inicPila (Pila* pila)
+        {
+            (*pila)=inicLista();
+        }
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+/// que hace esta funcion?
+int f(Pila* pila)
+{
+    int rta=0;
+    if ((*pila)==NULL)
+    {
+        rta = 1;
+    }
+
+    return rta;
+}
+*/
+
+
+
+#include "librerias/Pila.h"
+
+
+/// que error tiene esta funcion? escribir la funcion correcta
+int otroTope (Pila *pila)
+{
+    int rta=0;
+    if((*pila)!=NULL)
+    {
+        rta=verPrimero(*pila);
+    }
+
+    return rta;
+}
+
 int main()
 {
-    int i = 0;
-    int j = 0;
+    int a = 10;
+    {
+        int b = 20;
+        a = 20;
+        {
+            b = 30;
+            int c = 40;
+            {
+                b = 60;
+            }
+        }
 
-    i = ++i;
-    j = j++;
+//        c = 50;
+    }
+
+    printf("%i", a);
 
 
-    printf("++i: %i \n", i);
-    printf("j++: %i \n", j);
+
+}
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+/// esta funcion deberia crear un nodo, pero funciona mal. Corregirla
+void crearNodo(nodo* unNodo)
+{
+    nodo * aux = (nodo*)malloc(sizeof(nodo));
+    aux->dato = unNodo;
+
+    return aux;
+}
+
+/// esta funcion deberia crear un nodo doble, pero funcion mal.
+aux * crearNodo2(nodo* unNodo)
+{
+    nodo2* aux = (nodo2 *)malloc(sizeof(nodo2));
+    aux->dato=dato;
+    aux->ste=NULL;
+
+    return aux;
+}
+
+int main()
+{
+
 
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int tope (Pila * pila)
+{
+    int rta=0;
+    if((*pila)!=NULL)
+    {
+       rta=verPrimero(*pila);
+    }
+    return rta;
+}
+*/
---
+++ pasarInformacion/pasarInformacion.c
@@ -0,0 +1,115 @@
+#include "../librerias/pila.h"
+#include "../librerias/lista.h"
+
+void pasarInformacion_arregloArreglo(int arregloOrigen[], int validos, int arregloDestino[], int dimension)
+{
+    for(int i = 0; i < validos && i < dimension; i++)
+    {
+        arregloDestino[i] = arregloOrigen[i];
+    }
+}
+
+nodo* pasarInformacion_arregloLista(int arreglo[], int validos, nodo* lista)
+{
+    for(int i = 0; i < validos; i++)
+    {
+        lista = agregarAlPrincipio(lista, arreglo[i]);
+    }
+
+    return lista;
+}
+
+void pasarInformacion_arregloArchivo(int arreglo[], int validos, char nombreArchivo[])
+{
+    FILE* archivo = fopen(nombreArchivo, "wb");
+
+    if(archivo)
+    {
+        for(int i = 0; i < validos; i++)
+        {
+            fwrite(arreglo[i], sizeof(int), 1, archivo);
+        }
+
+        /// otra forma
+        /// fwrite(arreglo, sizeof(arreglo), 1, archivo);
+
+        fclose(archivo);
+    }
+}
+
+void pasarInformacion_arregloPila(int arreglo[], int validos, Pila* pila)
+{
+    for(int i = 0; i < validos; i++)
+    {
+        apilar(pila, arreglo[i]);
+    }
+}
+
+/// -----------------------------------------------------------
+
+typedef struct
+{
+    char nombre[30];
+    int DNI;
+} stPersona;
+
+void pasarInformacion_archivoArreglo(char* nombreArchivo, int arreglo[], int dimension)
+{
+    FILE* fArchivo = fopen(nombreArchivo, "rb");
+
+    if (fArchivo != NULL)
+    {
+        stPersona persona;
+        int i = 0;
+
+        while(fread(&persona, sizeof(persona), 1, fArchivo) > 0 && i < dimension)
+        {
+            arreglo[i] = persona;
+            i++;
+
+            /// tambien podria haber sido:
+            /// arreglo[i++] = persona;
+        }
+
+        fclose(fArchivo);
+    }
+}
+
+/**
+    Para el siguiente ejemplo utilizamos como parametro una variable de tipo FILE*
+
+    En el main llamariamos a la funcion de la siguiente manera:
+
+    nodo* lista = inicLista();
+    FILE* archivo = fopen("archivo.bin", "rb");
+
+    if(arhivo)
+    {
+        pasarInformacion_archivoLista(FILE* archivo, nodo* lista);
+
+        fclose(archivo);
+    }
+
+*/
+
+nodo* pasarInformacion_archivoLista(FILE* archivo, nodo* lista)
+{
+    stPersona persona;
+
+    while(fread(&persona, sizeof(persona), 1, archivo) > 0)
+    {
+        lista = agregarAlFinal(lista, persona);
+    }
+
+    return lista;
+}
+
+void pasarInformacion_archivoPila(FILE* archivo, Pila* pila)
+{
+    stPersona persona;
+
+    while(fread(&persona, sizeof(persona), 1, archivo) > 0)
+    {
+        apilar(pila, persona);
+    }
+}
